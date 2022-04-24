b1463c3ef3a8afa596c950fa9e8df0b6ef08c524
marcodupas <marco.dupas@protonmail.com>
2022-04-24 22:00:50 +0200

    save

--- scripts/parseCSV.ts
+++ scripts/parseCSV.ts
@@ -1,7 +1,9 @@
 const Papa = require('papaparse')
 const fs = require('fs')
 import { writeFile } from '$helpers/writeFile';
-import { regionalesCSVPath, regionalesOutputPath, departementalesCSVPath, departementalesOutputPath, europeennesCSVPath, europeennesOutputPath } from './config';
+import { slugify } from '$helpers/slugify';
+import { deputeJSONPath, regionalesCSVPath, regionalesOutputPath, departementalesCSVPath, departementalesOutputPath, europeennesCSVPath, europeennesOutputPath } from './config';
+import deputes from '../public/json/deputes.json';
 
 const readCSV = async (filePath) => {
   const csvFile = fs.readFileSync(filePath)
@@ -11,6 +13,7 @@ const readCSV = async (filePath) => {
     Papa.parse(csvData, {
       header: true,
       delimiter: ';',
+      // preview: 1,
       skipEmptyLines: true,
       complete: function(results) {
         resolve(results.data)
@@ -19,24 +22,50 @@ const readCSV = async (filePath) => {
   });
 };
 
-const removeUnwantedData = (json) => {
-  json.forEach((line)=> {
+const writeInDeputeFiles = async (json, label) => {
+  for (const line of json) {
     for (const key in line){
       if (line[key] === ""){
-        delete line[key]; 
+        delete line[key];
       }
     }
-  })
+    const county = line["Libellé du département"];
+    const circumscription = line["Code de la circonscription"];
+
+    if (county && circumscription) {
+      const matchingDepute = deputes.find((c) => c.county.toUpperCase() == county.toUpperCase() && c.circumscription == circumscription);
+      if (!matchingDepute) {
+        console.log('missing', county, circumscription);
+      } else {
+        const slug = slugify(matchingDepute.firstname + ' ' + matchingDepute.lastname);
+        const deputeFile = fs.readFileSync(deputeJSONPath + slug + '.json');
+        if(deputeFile){
+          
+          delete line['Code du département'];
+          delete line['Code de la circonscription'];
+          delete line['Libellé du département'];
+          let deputeFileContent = await JSON.parse(deputeFile)
+          const jsonToAdd = {[label]: line}
+          deputeFileContent = {...deputeFileContent, ...jsonToAdd}
+          await writeFile( deputeJSONPath + slug + '.json', JSON.stringify(deputeFileContent, null, 4))
+          console.log(label, slug, 'done')
+      
+        }
+      }
+    }
+  }
   return json
 }
 
-export const writeJSON = async (csvFilePath, outputPath) => {
+export const writeJSON = async (csvFilePath, outputPath, label) => {
   let parsedData = await readCSV(csvFilePath); 
-  let cleanData = await removeUnwantedData(parsedData)
-  writeFile( outputPath, JSON.stringify(cleanData))
-  console.log('Parsing of ', csvFilePath, 'completed with', cleanData.length, 'lines')
+  await writeInDeputeFiles(parsedData, label)
+  // writeFile( outputPath, JSON.stringify(cleanData))
+  // console.log('Parsing of ', csvFilePath, 'completed with', cleanData.length, 'lines')
 }
 
-writeJSON(regionalesCSVPath, regionalesOutputPath)
-writeJSON(departementalesCSVPath, departementalesOutputPath)
-writeJSON(europeennesCSVPath, europeennesOutputPath)
+(async function(){
+  await writeJSON(regionalesCSVPath, regionalesOutputPath, 'regionales')
+  await writeJSON(departementalesCSVPath, departementalesOutputPath, 'departementales')
+  await writeJSON(europeennesCSVPath, europeennesOutputPath, 'europeennes')
+})();
