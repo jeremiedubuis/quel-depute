b44dd0342ebf452eac7a58336c7c9f39f20b9508
Jérémie Dubuis <jeremie.dubuis@gmail.com>
2022-06-06 00:41:54 +0200

    :speaker_loud: log version

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,11 @@ import { SharedData } from '$components/layout/SharedData';
 import Head from 'next/head';
 import { Header } from '$components/layout/Header/Header';
 import { Footer } from '$components/layout/Footer/Footer';
+import pkg from '../package.json';
+console.log(pkg.version);
 export default function App({ Component, pageProps }) {
     return (
-        <RecoilRoot
-        >
+        <RecoilRoot>
             <Head>
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
             </Head>
