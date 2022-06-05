f357aa78c0870bd1c3e39b94ccfee77a3bd677ef
Jérémie Dubuis <jeremie.dubuis@gmail.com>
2022-06-05 10:36:43 +0200

    :lipstick:

--- components/modals/Modal.tsx
+++ components/modals/Modal.tsx
@@ -1,6 +1,7 @@
 import styles from './Modal.module.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { FiX } from 'react-icons/fi';
 
 export const Modal: React.FC<{
     children: React.ReactNode | React.ReactNode[];
@@ -10,6 +11,9 @@ export const Modal: React.FC<{
     return isVisible
         ? ReactDOM.createPortal(
               <div className={styles.modal} onClick={() => close()}>
+                  <button onClick={() => close()}>
+                      <FiX />
+                  </button>
                   <div onClick={(e) => e.stopPropagation()}>{children}</div>
               </div>,
               document.body
